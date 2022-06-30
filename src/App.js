3ae2994b5409cf869028d7760292e530e9bb10e5
studiomac-152 <studiomac-152@studiomac-152s-iMac.local>
2022-06-30 17:33:11 +0530

    Change in app.js remove switch and apply Hashrouter

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import HomePage from './pages/homepage/homepage';
 import signUp from './pages/sign-up/sign-up';
-import { Route, Router, Switch } from 'react-router-dom';
+import { HashRouter, Route, Router, Switch } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Switch>
+      <HashRouter>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/sign-up" component={signUp} />
-      </Switch>
+      </HashRouter>
     </div>
   );
 }
