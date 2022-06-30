88f9e5b8f507f9028ebdbfa9c7b2ccfb379bb78e
studiomac-152 <studiomac-152@studiomac-152s-iMac.local>
2022-06-30 16:57:30 +0530

    Create 404.html and few changes in index.html

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import HomePage from './pages/homepage/homepage';
 import signUp from './pages/sign-up/sign-up';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Router, Switch } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Router basename="/tothepoint_login">
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route exact path="/sign-up" component={signUp} />
         </Switch>
+      </Router>
     </div>
   );
 }
